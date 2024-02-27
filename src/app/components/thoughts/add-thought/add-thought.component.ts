4120974651057c04a3e5c591e60cc376e23937cc
Ubirajara Neves <biraneves@outlook.com>
2024-02-27 14:35:45 -0300

    Property binding

--- src/app/components/thoughts/add-thought/add-thought.component.ts
+++ src/app/components/thoughts/add-thought/add-thought.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-thought',
   templateUrl: './add-thought.component.html',
-  styleUrls: ['./add-thought.component.scss']
+  styleUrls: ['./add-thought.component.scss'],
 })
 export class AddThoughtComponent {
-
+  thought = {
+    id: '1',
+    content: 'Aprendendo Angular',
+    author: 'Dev',
+    model: '',
+  };
 }
