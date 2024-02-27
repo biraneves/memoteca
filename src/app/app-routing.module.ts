4a6692727c3b905e6cc57edac92fd8e383a2e6d9
Ubirajara Neves <biraneves@outlook.com>
2024-02-27 15:06:31 -0300

    Router navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddThoughtComponent } from './components/thoughts/add-thought/add-thought.component';
+import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'add-thought',
+    component: AddThoughtComponent,
+  },
+  {
+    path: 'list-thoughts',
+    component: ListThoughtsComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'list-thoughts',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
